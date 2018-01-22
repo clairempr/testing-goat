be1be50933d091b3ae1b30cc74d51d36da397675
Me <email>
2018-01-22 10:49:34 +0100

    Added check_for_row_in_list_table to FT

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Otis has heard about a cool new online to-do app. He goes
         # to check out its homepage
@@ -37,10 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Save souls" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Save souls', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Save souls')
 
         # There is still a text box inviting him to add another item. He
         # enters "Dig rifle pits" (You can never have too many rifle pits)
@@ -50,11 +52,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Save souls', [row.text for row in rows])
-        self.assertIn('2: Dig rifle pits', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Save souls')
+        self.check_for_row_in_list_table('2: Dig rifle pits')
 
         # Otis wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
