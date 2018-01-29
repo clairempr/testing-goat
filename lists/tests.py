47078539ba7a44ed1c0aad98004db9fc1a55cc07
Me <clairempr@users.noreply.github.com>
2018-01-29 11:16:40 +0100

    Adjusting the Rest of the World to Our New Models

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 from django.urls import resolve
@@ -7,15 +7,20 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 	
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
 
 		saved_items = Item.objects.all()
@@ -24,18 +29,15 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)		
 		self.assertEqual(second_saved_item.text, 'Item the second')
-
+		self.assertEqual(second_saved_item.list, list_)	
 
 class HomePageTest(TestCase):
 
-	def test_home_page_returns_correct_html(self):
+	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
-
-	def test_only_saves_items_when_necessary(self):
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
 
 
 class ListViewTest(TestCase):
@@ -45,8 +47,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		
