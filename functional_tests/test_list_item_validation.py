da7d08764f2af981285ce2c35094d00a1c1f296b
Me <clairempr@users.noreply.github.com>
2018-03-19 12:49:55 +0100

    End of Chapter 16

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,80 +10,80 @@ class ItemValidationTest(FunctionalTest):
     def get_error_element(self):
         return self.browser.find_element_by_css_selector('.has-error')
 
-        def test_cannot_add_empty_list_items(self):
-            # Otis goes to the home page and accidentally tries to submit
-            # an empty list item. He hits Enter on the empty input box
-            self.browser.get(self.live_server_url)
-            self.get_item_input_box().send_keys(Keys.ENTER)
+    def test_cannot_add_empty_list_items(self):
+        # Otis goes to the home page and accidentally tries to submit
+        # an empty list item. He hits Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-            # The browser intercepts the request, and does not load the
-            # list page
-            self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-                '#id_text:invalid'
-            ))
+        # The browser intercepts the request, and does not load the
+        # list page
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
 
-            # He starts typing some text for the new item and the error disappears
-            self.get_item_input_box().send_keys('Quit smoking')
-            self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-                '#id_text:valid'
-            ))
+        # He starts typing some text for the new item and the error disappears
+        self.get_item_input_box().send_keys('Quit smoking')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
 
-            # And he can submit it successfully
-            self.get_item_input_box().send_keys(Keys.ENTER)
-            self.wait_for_row_in_list_table('1: Quit smoking')
+        # And he can submit it successfully
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Quit smoking')
 
-            # Perversely, he now decides to submit a second blank list item
-            self.get_item_input_box().send_keys(Keys.ENTER)
+        # Perversely, he now decides to submit a second blank list item
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-            # Again, the browser will not comply
-            self.wait_for_row_in_list_table('1: Quit smoking')
-            self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-                '#id_text:invalid'
-            ))
+        # Again, the browser will not comply
+        self.wait_for_row_in_list_table('1: Quit smoking')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
 
-            # And he can correct it by filling some text in
-            self.get_item_input_box().send_keys('Buy opium')
-            self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-                '#id_text:valid'
-            ))
-            self.get_item_input_box().send_keys(Keys.ENTER)
-            self.wait_for_row_in_list_table('1: Quit smoking')
-            self.wait_for_row_in_list_table('2: Buy opium')
+        # And he can correct it by filling some text in
+        self.get_item_input_box().send_keys('Buy opium')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Quit smoking')
+        self.wait_for_row_in_list_table('2: Buy opium')
 
-        def test_cannot_add_duplicate_items(self):
-            # Otis goes to the home page and starts a new list
-            self.browser.get(self.live_server_url)
-            self.get_item_input_box().send_keys('Apologize to Lizzie')
-            self.get_item_input_box().send_keys(Keys.ENTER)
-            self.wait_for_row_in_list_table('1: Apologize to Lizzie')
+    def test_cannot_add_duplicate_items(self):
+        # Otis goes to the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Apologize to Lizzie')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Apologize to Lizzie')
 
-            # He accidentally tries to enter a duplicate item
-            self.get_item_input_box().send_keys('Apologize to Lizzie')
-            self.get_item_input_box().send_keys(Keys.ENTER)
+        # He accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Apologize to Lizzie')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-            # He sees a helpful error message
-            self.wait_for(lambda: self.assertEqual(
-                self.get_error_element().text,
-                "You've already got this in your list"
-            ))
+        # He sees a helpful error message
+        self.wait_for(lambda: self.assertEqual(
+            self.get_error_element().text,
+            "You've already got this in your list"
+        ))
 
-        def test_error_messages_are_cleared_on_input(self):
-            # Otis starts a list and causes a validation error:
-            self.browser.get(self.live_server_url)
-            self.get_item_input_box().send_keys('Neglect no precaution')
-            self.get_item_input_box().send_keys(Keys.ENTER)
-            self.wait_for_row_in_list_table('1: Neglect no precaution')
-            self.get_item_input_box().send_keys('Neglect no precaution')
-            self.get_item_input_box().send_keys(Keys.ENTER)
+    def test_error_messages_are_cleared_on_input(self):
+        # Otis starts a list and causes a validation error:
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Neglect no precaution')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Neglect no precaution')
+        self.get_item_input_box().send_keys('Neglect no precaution')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-            self.wait_for(lambda: self.assertTrue(
-                self.get_error_element().is_displayed()
-            ))
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
 
-            # He starts typing in the input box to clear the error
-            self.get_item_input_box().send_keys('a')
+        # He starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
 
-            # He is pleased to see that the error message disappears
-            self.wait_for(lambda: self.assertFalse(
-                self.get_error_element().is_displayed()
-            ))
+        # He is pleased to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
